03950453a1d8153b8caa7a029ac11714d6c4c7a4
alifarahani1998 <alifarahani1998@gmail.com>
2020-01-25 16:21:16 +0330

    this is the second commit for basic MVC Model

--- src/main/java/com/example/linkedin/LinkedinApplication.java
+++ src/main/java/com/example/linkedin/LinkedinApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LinkedinApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(LinkedinApplication.class, args);
     }
--- src/main/java/com/example/linkedin/SignUpController.java
+++ src/main/java/com/example/linkedin/SignUpController.java
@@ -1,15 +1,15 @@
 package com.example.linkedin;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Controller
+public class SignUpController {
 
-@RestController
-public class mainController {
-
-    @RequestMapping("/login")
+    @RequestMapping("/signUp")
     public String controller () {
-
-        return "Welcome To Linkedin new version!";
+        return "SignUp.jsp";
     }
+
 }
